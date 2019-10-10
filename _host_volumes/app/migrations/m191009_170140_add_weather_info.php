90b57639c52981d47cdf4beb639f8827fbc072a5
Alexandr <riskeralex@gmail.com>
2019-10-10 14:24:46 +0500

    Add weather migrations

---
+++ _host_volumes/app/migrations/m191009_170140_add_weather_info.php
@@ -0,0 +1,49 @@
+<?php
+
+use yii\db\Migration;
+use yii\db\Schema;
+
+/**
+ * Class m191009_170140_add_weather_info
+ */
+class m191009_170140_add_weather_info extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+		$this->createTable('weather',[
+			'id' => Schema::TYPE_PK,
+            'city' => Schema::TYPE_STRING . ' NOT NULL',
+            'temp' => Schema::TYPE_STRING . ' NOT NULL',
+		]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('weather');
+		
+		echo "m191009_170140_add_weather_info cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m191009_170140_add_weather_info cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
