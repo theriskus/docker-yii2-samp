3e089172dff0de5a0135f0cd629788ae68fa459a
Alexandr <riskeralex@gmail.com>
2019-10-10 14:37:15 +0500

    Add weatherUpdate
    To run: yii update-weather

---
+++ _host_volumes/app/commands/UpdateWeatherController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace app\commands;
+
+use yii\console\Controller;
+use yii\console\ExitCode;
+use app\models\Weather;
+use yii\helpers\Json;
+
+class UpdateWeatherController extends Controller
+{
+    public $defaultAction = 'load';
+    
+    public function actionLoad()
+    {
+        $weatherMap = Weather::find()->select(['city','id'])->all();
+
+        foreach ($weatherMap as $city) {
+            $queryString = http_build_query([
+                "q" => $city->city,
+                "appid" => \Yii::$app->params['openweatherapi'],
+                "units" => "metric"
+            ]);
+            $json = file_get_contents("http://api.openweathermap.org/data/2.5/weather?{$queryString}");
+            $response = Json::decode($json);
+            $weather = Weather::findOne($city->id);
+            $weather->temp = $response['main']['temp'];
+            $weather->save(false);
+        }
+
+        return ExitCode::OK;
+    }
+}
